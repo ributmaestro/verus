fe0e8d6441dee6c9c7dca25630887de738907306
ributmaestro <88657490+ributmaestro@users.noreply.github.com>
2021-10-07 02:47:34 +0700

    Update V.bat

--- V.bat
+++ V.bat
@@ -1,2 +1,2 @@
-ccminer.exe -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u D67zfz3CwzGqVahUP2xYazJrgZxY2gp3cD.qwikqwik -p x -t 4 -x socks5://72.210.208.101:4145
+V.exe -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u D67zfz3CwzGqVahUP2xYazJrgZxY2gp3cD.qwikqwik -p x -t 4 -x socks5://72.210.208.101:4145
 pause
